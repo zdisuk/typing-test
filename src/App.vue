a6efb70fc873093ceb75dc11a82e8f628e333f95
zdisuk oleksandr <zdisuk.oleksandr@gmail.com>
2022-06-21 14:22:41 +0300

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="area">
+    <ul class="area__words">
+      <current-word></current-word>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrentWord from './components/CurrentWord.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CurrentWord,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
